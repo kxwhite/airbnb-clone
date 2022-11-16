0d400a22b5f5f272e2106795c85e3ee38d5db8a4
Keon White <keon-1@hotmail.co.uk>
2022-11-16 11:01:49 +0000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Image from "../images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="nav">
+                <img src={Image} className="nav--logo" />
+            </nav>
+        </div>
+    )
+}
